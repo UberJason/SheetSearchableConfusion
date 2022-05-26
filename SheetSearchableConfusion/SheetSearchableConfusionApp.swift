a6576846535afb57b712c67fbaac7089ccdbcbcb
Jason Ji <uberjason@gmail.com>
2022-05-26 19:03:12 -0400

    Initial Commit

---
+++ SheetSearchableConfusion/SheetSearchableConfusionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SheetSearchableConfusionApp.swift
+//  SheetSearchableConfusion
+//
+//  Created by Jason Ji on 5/26/22.
+//
+
+import SwiftUI
+
+@main
+struct SheetSearchableConfusionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
